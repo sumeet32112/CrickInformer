bcb1ff0779c8ce90e489f2a1d4b7a48cb7b1fa24
sumeet32112 <sumeetsingh32112@gmail.com>
2024-02-27 16:17:11 +0530

    live matches api call completed

--- cricInformer-frontend/src/app/pages/live/live.component.ts
+++ cricInformer-frontend/src/app/pages/live/live.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiCallService } from '../../services/api-call.service';
+import { MatchCardComponent } from '../../components/match-card/match-card.component';
 
 @Component({
   selector: 'app-live',
   standalone: true,
-  imports: [],
+  imports: [MatchCardComponent],
   templateUrl: './live.component.html',
   styleUrl: './live.component.css'
 })
-export class LiveComponent {
+export class LiveComponent implements OnInit{
 
+  liveMatches:any;
+  constructor(private _api : ApiCallService){
+
+  }
+  ngOnInit(): void {
+    this.loadLiveMatches();
+  }
+
+
+  private loadLiveMatches() {
+    this._api.getLiveMatches().subscribe({
+      next: data => {
+        console.log(data);
+        this.liveMatches = data;
+      },
+      error: error => {
+        console.log(error);
+      }
+    });
+  }
 }
